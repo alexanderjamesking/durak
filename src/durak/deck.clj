67d816a4db10971a9fdff4ae59f7e7a66fc4b26f
Alex King <alex@alexanderjamesking.com>
2017-02-11 13:08:45 +0000

    Initial commit with basic card deck implemented

---
+++ src/durak/deck.clj
@@ -0,0 +1,35 @@
+(ns durak.deck)
+
+(def suits (set '(:spades :hearts :diamonds :clubs)))
+
+(defn value-symbol [card-value]
+  "Takes a card value and returns its symbol, e.g. J for 11, Q for 12"
+  (case card-value
+    11 "J"
+    12 "Q"
+    13 "K"
+    14 "A"
+    card-value))
+
+(defn suit-symbol [card-suit]
+  "Takes a card suit and returns its symbol, e.g. ♠ for :spades"
+  (case card-suit
+    :spades "♠"
+    :hearts "♥"
+    :diamonds "♦"
+    :clubs "♣"
+    nil))
+
+(defn build-card [suit value]
+  {:suit suit :value value})
+
+(defn build-suit [suit]
+  (set (map #(build-card suit %) (range 6 15))))
+
+(defn card-symbol [card]
+  (let [value (:value card)
+        suit (:suit card)]
+    (str (value-symbol value) (suit-symbol suit))))
+
+(defn build-deck []
+  (reduce concat (map build-suit suits)))
---
+++ test/durak/deck_test.clj
@@ -0,0 +1,64 @@
+(ns durak.deck-test
+  (:require [clojure.test :refer :all]
+            [durak.deck :refer :all]))
+
+(deftest build-deck-of-cards
+  (testing "build card"
+    (let [card (build-card :hearts 12)]
+      (is (= :hearts (:suit card)))
+      (is (= 12 (:value card)))))
+
+  (testing "card-symbol"
+    (is (= "Q♥" (card-symbol (build-card :hearts 12))))
+    (is (= "10♥" (card-symbol (build-card :hearts 10)))))
+
+  (testing "suit contains 9 cards"
+    (let [diamonds (build-suit :diamonds)]
+      (is (= 9 (count diamonds)))))
+
+  (testing "build deck"
+    (let [deck (build-deck)]
+      (is (= 36 (count deck)))))
+
+  (testing "deck to symbols"
+    (let [deck (set (map #(card-symbol %) (build-deck)))]
+      (is (= 36 (count deck)))
+      (assert (contains? deck "6♥"))
+      (assert (contains? deck "7♥"))
+      (assert (contains? deck "8♥"))
+      (assert (contains? deck "9♥"))
+      (assert (contains? deck "10♥"))
+      (assert (contains? deck "J♥"))
+      (assert (contains? deck "Q♥"))
+      (assert (contains? deck "K♥"))
+      (assert (contains? deck "A♥"))
+
+      (assert (contains? deck "6♠"))
+      (assert (contains? deck "7♠"))
+      (assert (contains? deck "8♠"))
+      (assert (contains? deck "9♠"))
+      (assert (contains? deck "10♠"))
+      (assert (contains? deck "J♠"))
+      (assert (contains? deck "Q♠"))
+      (assert (contains? deck "K♠"))
+      (assert (contains? deck "A♠"))
+
+      (assert (contains? deck "6♦"))
+      (assert (contains? deck "7♦"))
+      (assert (contains? deck "8♦"))
+      (assert (contains? deck "9♦"))
+      (assert (contains? deck "10♦"))
+      (assert (contains? deck "J♦"))
+      (assert (contains? deck "Q♦"))
+      (assert (contains? deck "K♦"))
+      (assert (contains? deck "A♦"))
+
+      (assert (contains? deck "6♣"))
+      (assert (contains? deck "7♣"))
+      (assert (contains? deck "8♣"))
+      (assert (contains? deck "9♣"))
+      (assert (contains? deck "10♣"))
+      (assert (contains? deck "J♣"))
+      (assert (contains? deck "Q♣"))
+      (assert (contains? deck "K♣"))
+      (assert (contains? deck "A♣")))))
